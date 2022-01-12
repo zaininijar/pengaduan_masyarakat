7a62d48389406adbff817766c9389eb99adaf075
zaininijar <zaininijar7610@gmail.com>
2022-01-13 06:18:01 +0700

    13/01/2

--- app/welcome.php
+++ app/welcome.php
@@ -37,38 +37,76 @@
             <span class="visually-hidden">Next</span>
         </button>
     </div>
-    <div class="container my-5">
-        <div class="row text-center">
-            <h2 class="pb-5">Features</h2>
+    <section class="bg-light">
+        <div class="container my-5 pb-5">
+            <div class="row text-center">
+                <h2 class="py-5">Features</h2>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mt-3 md-mt-0">
+                        <div class="card-body">
+                            <h5 class="card-title">Aman</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mt-3 md-mt-0">
+                        <div class="card-body">
+                            <h5 class="card-title">Langusng</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mt-3 md-mt-0">
+                        <div class="card-body">
+                            <h5 class="card-title">Terpercaya</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card mt-3 md-mt-0">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </section>
+    <section>
+        <div class="container my-5 pt-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-landing">
+                        <h2>Keluhan dapat mudah tersampaikan tanpa perantara</h2>
+                        <p class="mt-5">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, odit atque. Quod sit commodi eaque quaerat unde, expedita voluptatibus. Exercitationem, perferendis temporibus voluptates ut provident dignissimos nobis excepturi officia quas!
+                        </p>
+                        <a href="#" class="btn btn-primary">Segera Mengadu</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mt-3 md-mt-0">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+                <div class="col-md-6">
+                    <div class="image-landing" style="background-image: url(<?= $base_url . 'assets/images/landing-page/team-chat.svg' ?>);"></div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card mt-3 md-mt-0">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </section>
+    <section class="bg-light">
+        <div class="container my-5 pt-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="image-landing" style="background-image: url(<?= $base_url . 'assets/images/landing-page/privacy.svg' ?>);"></div>
+                </div>
+                <div class="col-md-6">
+                    <div class="text-landing">
+                        <h2>Privasi pengadu dapat terjaga dan terjamin kerahasiaannya</h2>
+                        <p class="mt-5">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, odit atque. Quod sit commodi eaque quaerat unde, expedita voluptatibus. Exercitationem, perferendis temporibus voluptates ut provident dignissimos nobis excepturi officia quas!
+                        </p>
+                        <a href="#" class="btn btn-primary">Segera Mengadu</a>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
