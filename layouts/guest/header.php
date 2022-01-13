7a5f3f3595ed64c6282b053faf7bba87fd354d03
zaini nijar <wevex28@gmail.com>
2022-01-13 10:55:44 +0700

    01/13/22

--- layouts/guest/header.php
+++ layouts/guest/header.php
@@ -17,6 +17,17 @@
             <div class="container">
                 <a href="<?= $base_url; ?>" class="navbar-brand"
                     style="background-image: url(<?= $base_url . 'assets/appmas-logo/cover.png' ?>);"></a>
+                <?php if(isset($_SESSION['auth'])): ?>
+                <?php if(isset($_SESSION['auth']) && $_SESSION['auth']['role_id'] == 1): ?>
+                <a href="admin/index">
+                    <button class="btn btn-outline-primary px-3">Dashboard</button>
+                </a>
+                <?php else: ?>
+                <a href="masyarakat/index">
+                    <button class="btn btn-outline-primary px-3">Dashboard</button>
+                </a>
+                <?php endif; ?>
+                <?php else: ?>
                 <div class="d-flex">
                     <?php if(isset($url)): ?>
                     <?php if($url[0] == 'login'): ?>
@@ -43,6 +54,7 @@
                     </a>
                     <?php endif; ?>
                 </div>
+                <?php endif; ?>
             </div>
         </nav>
     </header>
